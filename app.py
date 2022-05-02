20d8da03e53667d031ed577602808143a51c450a
pankhurimishra81 <83502670+pankhurimishra81@users.noreply.github.com>
2022-05-02 22:07:22 +0530

    first commit
    committing first files

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+import warnings
+import pandas as pd
+warnings.filterwarnings("ignore")
+
+# Importing model library
+import model
+
+
+# Initializing Flask App
+app = Flask(__name__)
+
+
+# Defining method for root path
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/recommend-products', methods=['POST'])
+def recommendProducts():
+    # Fetching the user entered username 
+    username =  str(request.form.get('username'))
+
+    # Getting recommendations for user using the model
+    modelOutput = model.sentimentBasedProductRecommendations(username)
+
+    # Checking type of variable if it's not a list then it will be an error
+    if type(modelOutput) != list : 
+        # Returning the error to be displayed
+        return render_template('index.html', error=modelOutput)
+
+    # If output is a list then:
+    # Converting recommendations list to a dataFrame
+    recommendations = pd.DataFrame(modelOutput, columns=['Product Recommendations'])
+
+    # Creating a HTML table using DataFrame
+    recommendations_table = [recommendations.to_html(classes='recommendations')]
+    # Defining Table title
+    table_title = ['NAN', 'Top 5 Product Recommendations for : {}'.format(username)]
+
+    # Rendering Template while passing the results
+    return  render_template('index.html', productsTable = recommendations_table, titles=table_title)
+
+@app.route('/allusernames', methods=['GET'])
+def allUsernames():
+    # Reading usernames from source i.e. recommendation system
+    usernamesList = list(model.ProductRecommendationSystem.index)
+
+    # Converting List to a dataframe
+    usernamesDF = pd.DataFrame(usernamesList, columns=['Available Usernames'])
+
+    # Creating a HTML table using DataFrame
+    usernameTable = [usernamesDF.to_html(classes='usernames')]
+    # Defining Table title
+    table_title = ['NAN', 'List of all usernames available in SBPRS Database']
+
+    # Rendering the page
+    return render_template('allusernames.html', usernameTable = usernameTable, titles = table_title )
+
+
+# Starting Flask Application
+if __name__ == '__main__' :
+    app.run()
---
+++ model.py
@@ -0,0 +1,81 @@
+# Importing required libraries
+import pandas as pd
+import numpy as np
+import pickle
+
+# Importing constants for loading required variables
+import constants
+
+# Loading Recommendation System
+ProductRecommendationSystem = pickle.load(file=open(constants.RECOMMENDATION_SYSTEM_PATH, 'rb'))
+print("ProductRecommendationSystem")
+# Loading ProductReviewMapping (TRAIN)
+processedProductReviewMapping = pd.read_csv(constants.PRODUCT_REVIEWS_MAPPING_PATH)
+print("processedProductReviewMapping", processedProductReviewMapping.shape)
+# Loading Vectorizer
+vectorizer = pickle.load(file=open(constants.VECTORIZER_PATH, 'rb'))
+print("loading vectorizer")
+# Loading Sentiment Classifier
+SentimentClassifier = pickle.load(file=open(constants.SENTIMENT_CLASSIFIER_PATH, 'rb'))
+print("loading SentimentClassifier")
+# Loading the Probability Threshold
+probThreshold = constants.PROBABILITY_THRESHOLD
+print("probThreshold")
+
+
+
+# Defining function for Sentiment based Product Recommendations
+def sentimentBasedProductRecommendations(username):
+    '''
+    This function takes username as input and
+    generates top 5 product recommendations based on their preferences
+    ::Uses Item-based Recommendation System::
+    '''
+    
+    # Fetching top 20 recommendations for user
+    try:
+        top20_recommendations = (ProductRecommendationSystem
+                                 .loc[username]
+                                 .sort_values(ascending=False)[:20])
+    except KeyError:
+        # If user doesn't exist print the error message
+        errorMessage = "ERROR: Unable to recommend products to username '{}', as it doesn't not exist in the system!\n\
+            Please try again with a user from 'Available Users' list provided above. ".format(username)
+        
+        return errorMessage
+    
+    # Creating a Dictionary to store the percentage of positive sentiments
+    recommendationsPositivePercentage = {}
+    
+    # Iterating over all the recommendations
+    for recommendedProduct in top20_recommendations.index:
+        
+        # Filtering out the reviews_text from the Training Dataset
+        filteredReviews = (processedProductReviewMapping
+                           [processedProductReviewMapping['name'] == recommendedProduct]
+                           ['reviews_text'])
+        
+        # Using Vectorizer to transform the filtered reviews
+        vectorizedReviews = vectorizer.transform(filteredReviews)
+        
+        # Getting the sentiment probability using vectorized reviews
+        sentimentProba = SentimentClassifier.predict_proba(vectorizedReviews).T[1]
+        # Using probability threshold to label predictions
+        reviewsSentiment = [1 if val>probThreshold else 0 for val in sentimentProba]
+        
+        # Calculating the percentatge of positive reviews
+        positivePercentage = sum(reviewsSentiment) / len(reviewsSentiment)
+        
+        # Logging in the dictionary
+        recommendationsPositivePercentage[recommendedProduct] = positivePercentage
+        
+    # Sorting the dictionary based on values in descending
+    sortedByPositivePercentage = dict(sorted(recommendationsPositivePercentage.items(),
+                                             key=lambda x : x[1],
+                                             reverse=True))
+    
+    # Storing the recommendations in the required order and returning the top 5 recommendations
+    recommendations = list(sortedByPositivePercentage.keys())[:5]
+    
+    # Returning the recommendations
+    return recommendations
